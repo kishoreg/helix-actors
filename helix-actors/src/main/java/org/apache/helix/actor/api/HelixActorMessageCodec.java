0a5d33d313839c8a965e70e5eb2c14b7c6d48fb1
Greg Brandt <brandt.greg@gmail.com>
2014-07-18 07:58:19 -0700

    added some docs to codec interface

--- helix-actors/src/main/java/org/apache/helix/actor/api/HelixActorMessageCodec.java
+++ helix-actors/src/main/java/org/apache/helix/actor/api/HelixActorMessageCodec.java
@@ -6,6 +6,35 @@ import io.netty.buffer.ByteBuf;
  * Encodes and decodes messages of type T
  */
 public interface HelixActorMessageCodec<T> {
+    /**
+     * Encodes a typed message into a {@link io.netty.buffer.ByteBuf}.
+     *
+     * <p>
+     *     {@link io.netty.buffer.ByteBuf#release()} will be called once on the
+     *     return value, so if you want to ensure that it doesn't get
+     *     reclaimed, call {@link io.netty.buffer.ByteBuf#retain()} before
+     *     returning it.
+     * </p>
+     */
     ByteBuf encode(T message);
+
+    /**
+     * Decodes a typed message from a {@link io.netty.buffer.ByteBuf}.
+     *
+     * <p>
+     *     The reader index will be positioned at the beginning of the message,
+     *     and the next {@link io.netty.buffer.ByteBuf#readableBytes()} are the
+     *     message.
+     * </p>
+     *
+     * <p>
+     *     For example, if T is String, and you want to generate a new object:
+     *     <pre>
+     *         byte[] bytes = new byte[message.readableBytes()];
+     *         message.readBytes(bytes);
+     *         return new String(bytes);
+     *     </pre>
+     * </p>
+     */
     T decode(ByteBuf message);
 }
