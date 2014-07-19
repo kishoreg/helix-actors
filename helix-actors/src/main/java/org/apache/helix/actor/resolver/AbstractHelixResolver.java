9902be441b6585cf651655d0935ad92bf8ee7b02
Kanak Biscuitwala <kanak.b@hotmail.com>
2014-07-18 17:15:21 -0700

    Initial take on resolvers

---
+++ helix-actors/src/main/java/org/apache/helix/actor/resolver/AbstractHelixResolver.java
@@ -0,0 +1,256 @@
+package org.apache.helix.actor.resolver;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import org.apache.helix.HelixDataAccessor;
+import org.apache.helix.HelixManager;
+import org.apache.helix.NotificationContext;
+import org.apache.helix.model.ExternalView;
+import org.apache.helix.model.InstanceConfig;
+import org.apache.log4j.Logger;
+
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A basic implementation of a resolver in terms of expiring routing tables
+ * TODO: take a long look at synchronization and monitor granularity
+ */
+public abstract class AbstractHelixResolver implements HelixResolver {
+  private static final Logger LOG = Logger.getLogger(AbstractHelixResolver.class);
+  private static final int DEFAULT_THREAD_POOL_SIZE = 10;
+  private static final long DEFAULT_LEASE_LENGTH_MS = 60 * 60 * 1000; // TODO: are these good
+                                                                      // values?
+  private static final String ACTOR_PORT = "ACTOR_PORT";
+  private final Map<String, Spectator> _connections;
+  private boolean _isConnected;
+  private ScheduledExecutorService _executor;
+
+  protected AbstractHelixResolver() {
+    _connections = Maps.newHashMap();
+    _isConnected = false;
+  }
+
+  @Override
+  public void connect() {
+    _executor = Executors.newScheduledThreadPool(DEFAULT_THREAD_POOL_SIZE);
+    _isConnected = true;
+  }
+
+  @Override
+  public void disconnect() {
+    synchronized (_connections) {
+      for (Spectator connection : _connections.values()) {
+        connection.shutdown();
+      }
+      _connections.clear();
+    }
+    _executor.shutdown();
+    _isConnected = false;
+  }
+
+  @Override
+  public Set<InetSocketAddress> resolve(HelixMessageScope scope) {
+    if (!scope.isValid()) {
+      LOG.error("Scope " + scope + " is not valid!");
+      return Collections.emptySet();
+    } else if (!_isConnected) {
+      LOG.error("Cannot resolve " + scope + " without first connecting!");
+      return Collections.emptySet();
+    }
+
+    // Connect or refresh connection
+    String cluster = scope.getCluster();
+    ResolverRoutingTable routingTable;
+    synchronized (_connections) {
+      Spectator connection;
+      if (_connections.containsKey(cluster)) {
+        connection = _connections.get(cluster);
+      } else {
+        connection = new Spectator(cluster, DEFAULT_LEASE_LENGTH_MS);
+        connection.init();
+        _connections.put(cluster, connection);
+      }
+      routingTable = connection.getRoutingTable();
+    }
+
+    // Resolve all resources, either explicitly or match all
+    Set<String> resources;
+    if (scope.getResource() != null) {
+      resources = Sets.newHashSet(scope.getResource());
+    } else {
+      resources = routingTable.getResources();
+    }
+
+    // Resolve all partitions
+    Map<String, Set<String>> partitionMap = Maps.newHashMap();
+    if (scope.getPartition() != null) {
+      for (String resource : resources) {
+        partitionMap.put(resource, Sets.newHashSet(scope.getPartition()));
+      }
+    } else {
+      for (String resource : resources) {
+        partitionMap.put(resource, routingTable.getPartitions(resource));
+      }
+    }
+
+    // Resolve all states
+    Set<String> states;
+    if (scope.getState() != null) {
+      states = Sets.newHashSet(scope.getState());
+    } else {
+      states = routingTable.getStates();
+    }
+
+    // Get all the participants that match
+    Set<InstanceConfig> participants = Sets.newHashSet();
+    for (String resource : resources) {
+      for (String partition : partitionMap.get(resource)) {
+        for (String state : states) {
+          participants.addAll(routingTable.getInstances(resource, partition, state));
+        }
+      }
+    }
+
+    // Resolve those participants
+    Set<InetSocketAddress> result = Sets.newHashSet();
+    for (InstanceConfig participant : participants) {
+      String actorPort = participant.getRecord().getSimpleField(ACTOR_PORT);
+      if (actorPort == null) {
+        LOG.error("No actor address registered for target instance "
+            + participant.getInstanceName() + ", skipping");
+      } else {
+        result.add(new InetSocketAddress(participant.getHostName(), Integer.valueOf(actorPort)));
+      }
+    }
+    return result;
+  }
+
+  @Override
+  public boolean isConnected() {
+    return _isConnected;
+  }
+
+  /**
+   * Create a Helix manager connection based on the appropriate backing store
+   * @param cluster the name of the cluster to connect to
+   * @return HelixManager instance
+   */
+  protected abstract HelixManager createManager(String cluster);
+
+  private class Spectator {
+    private final String _cluster;
+    private final HelixManager _manager;
+    private final ResolverRoutingTable _routingTable;
+    private final long _leaseLengthMs;
+    private ScheduledFuture<?> _future;
+
+    /**
+     * Initialize a spectator. This does not automatically connect.
+     * @param cluster the cluster to spectate
+     * @param leaseLengthMs the expiry of this spectator after the last request
+     */
+    public Spectator(String cluster, long leaseLengthMs) {
+      _cluster = cluster;
+      _manager = createManager(cluster);
+      _leaseLengthMs = leaseLengthMs;
+      _routingTable = new ResolverRoutingTable();
+    }
+
+    /**
+     * Connect and initialize the routing table
+     */
+    public void init() {
+      try {
+        _manager.connect();
+        _manager.addExternalViewChangeListener(_routingTable);
+        _manager.addInstanceConfigChangeListener(_routingTable);
+
+        // Force an initial refresh
+        HelixDataAccessor accessor = _manager.getHelixDataAccessor();
+        List<ExternalView> externalViews =
+            accessor.getChildValues(accessor.keyBuilder().externalViews());
+        NotificationContext context = new NotificationContext(_manager);
+        context.setType(NotificationContext.Type.INIT);
+        _routingTable.onExternalViewChange(externalViews, context);
+        List<InstanceConfig> instanceConfigs =
+            accessor.getChildValues(accessor.keyBuilder().instanceConfigs());
+        _routingTable.onInstanceConfigChange(instanceConfigs, context);
+      } catch (Exception e) {
+        LOG.error("Error setting up routing table", e);
+      }
+    }
+
+    /**
+     * Clean up the connection to the spectated cluster
+     */
+    public void shutdown() {
+      resetFuture();
+      expire();
+    }
+
+    /**
+     * Get the dynamically-updating routing table for this cluster
+     * @return ResolverRoutingTable, a RoutingTableProvider that can answer questions about its
+     *         contents
+     */
+    public ResolverRoutingTable getRoutingTable() {
+      renew();
+      return _routingTable;
+    }
+
+    private synchronized void renew() {
+      resetFuture();
+
+      // Schedule this connection to expire if not renewed quickly enough
+      _future = _executor.scheduleWithFixedDelay(new Runnable() {
+        @Override
+        public void run() {
+          expire();
+        }
+      }, 0L, _leaseLengthMs, TimeUnit.MILLISECONDS);
+
+    }
+
+    private synchronized void resetFuture() {
+      if (_future != null && !_future.isDone()) {
+        _future.cancel(true);
+      }
+    }
+
+    private void expire() {
+      synchronized (_connections) {
+        _connections.remove(_cluster);
+        if (_manager != null && _manager.isConnected()) {
+          _manager.disconnect();
+        }
+      }
+    }
+  }
+}
---
+++ helix-actors/src/test/java/org/apache/helix/actor/resolver/TestZKHelixResolver.java
@@ -0,0 +1,136 @@
+package org.apache.helix.actor.resolver;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import org.apache.helix.HelixAdmin;
+import org.apache.helix.ZkUnitTestBase;
+import org.apache.helix.actor.resolver.zk.ZKHelixResolver;
+import org.apache.helix.integration.manager.ClusterControllerManager;
+import org.apache.helix.integration.manager.MockParticipantManager;
+import org.apache.helix.model.ExternalView;
+import org.apache.helix.model.IdealState;
+import org.apache.helix.model.InstanceConfig;
+import org.apache.helix.tools.ClusterSetup;
+import org.apache.helix.tools.ClusterStateVerifier;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Test basic routing table lookups for a ZK-based Helix resolver.
+ */
+public class TestZKHelixResolver extends ZkUnitTestBase {
+  private static final int NUM_PARTICIPANTS = 2;
+  private static final int NUM_PARTITIONS = 2;
+  private static final String CLUSTER_NAME = TestZKHelixResolver.class.getSimpleName();
+  private static final String RESOURCE_NAME = "MyResource";
+  private MockParticipantManager[] _participants;
+  private ClusterControllerManager _controller;
+  private ClusterSetup _setupTool;
+  private HelixResolver _resolver;
+  private Map<String, InetSocketAddress> _socketMap;
+
+  @BeforeClass
+  public void beforeClass() {
+    // Set up cluster
+    _setupTool = new ClusterSetup(_gZkClient);
+    _setupTool.addCluster(CLUSTER_NAME, true);
+    _setupTool.addResourceToCluster(CLUSTER_NAME, RESOURCE_NAME, NUM_PARTITIONS, "OnlineOffline",
+        IdealState.RebalanceMode.FULL_AUTO.toString());
+    _setupTool.rebalanceCluster(CLUSTER_NAME, RESOURCE_NAME, 1, RESOURCE_NAME, null);
+
+    // Set up and start instances
+    _socketMap = Maps.newHashMap();
+    HelixAdmin admin = _setupTool.getClusterManagementTool();
+    _participants = new MockParticipantManager[NUM_PARTICIPANTS];
+    for (int i = 0; i < NUM_PARTICIPANTS; i++) {
+      String host = "localhost";
+      int port = i;
+      int actorPort = i + 100;
+      String instanceName = host + "_" + port;
+      InstanceConfig config = new InstanceConfig(instanceName);
+      config.setHostName(host);
+      config.setPort(Integer.toString(port));
+      config.getRecord().setSimpleField("ACTOR_PORT", Integer.toString(actorPort));
+      admin.addInstance(CLUSTER_NAME, config);
+      _socketMap.put(instanceName, new InetSocketAddress(host, actorPort));
+      _participants[i] = new MockParticipantManager(ZK_ADDR, CLUSTER_NAME, instanceName);
+      _participants[i].syncStart();
+    }
+
+    // Start controller
+    _controller = new ClusterControllerManager(ZK_ADDR, CLUSTER_NAME, "controller_0");
+    _controller.syncStart();
+
+    // Connect a resolver
+    _resolver = new ZKHelixResolver(ZK_ADDR);
+    _resolver.connect();
+
+    // Wait for External view convergence
+    ClusterStateVerifier.verifyByZkCallback(new ClusterStateVerifier.BestPossAndExtViewZkVerifier(
+        ZK_ADDR, CLUSTER_NAME), 10000);
+  }
+
+  @Test
+  public void testResolution() {
+    HelixMessageScope clusterScope = new HelixMessageScope.Builder().cluster(CLUSTER_NAME).build();
+    Set<InetSocketAddress> clusterScopedInstances = _resolver.resolve(clusterScope);
+    Assert.assertTrue(clusterScopedInstances.containsAll(_socketMap.values()), "Expected "
+        + _socketMap.values() + ", found " + clusterScopedInstances);
+
+    HelixMessageScope resourceScope =
+        new HelixMessageScope.Builder().cluster(CLUSTER_NAME).resource(RESOURCE_NAME).build();
+    Set<InetSocketAddress> resourceScopedInstances = _resolver.resolve(resourceScope);
+    Assert.assertTrue(resourceScopedInstances.containsAll(_socketMap.values()), "Expected "
+        + _socketMap.values() + ", found " + resourceScopedInstances);
+
+    HelixMessageScope partition0Scope =
+        new HelixMessageScope.Builder().cluster(CLUSTER_NAME).resource(RESOURCE_NAME)
+            .partition(RESOURCE_NAME + "_0").build();
+    Set<InetSocketAddress> partition0ScopedInstances = _resolver.resolve(partition0Scope);
+    ExternalView externalView =
+        _setupTool.getClusterManagementTool().getResourceExternalView(CLUSTER_NAME, RESOURCE_NAME);
+    Set<String> instanceSet = externalView.getStateMap(RESOURCE_NAME + "_0").keySet();
+    Set<InetSocketAddress> expectedSocketAddrs = Sets.newHashSet();
+    for (String instanceName : instanceSet) {
+      expectedSocketAddrs.add(_socketMap.get(instanceName));
+    }
+    Assert.assertTrue(partition0ScopedInstances.equals(expectedSocketAddrs), "Expected "
+        + expectedSocketAddrs + ", found " + partition0ScopedInstances);
+
+  }
+
+  @AfterClass
+  public void afterClass() {
+    _resolver.disconnect();
+    _controller.syncStop();
+    for (MockParticipantManager participant : _participants) {
+      participant.syncStop();
+    }
+  }
+
+}
