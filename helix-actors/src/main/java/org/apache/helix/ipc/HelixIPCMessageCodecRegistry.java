8ff6552ce2c9c8ec18f81e495affea5373686344
Greg Brandt <brandt.greg@gmail.com>
2014-07-23 11:33:06 -0700

    reserve 1024 message types for internal use

--- helix-actors/src/main/java/org/apache/helix/ipc/HelixIPCMessageCodecRegistry.java
+++ helix-actors/src/main/java/org/apache/helix/ipc/HelixIPCMessageCodecRegistry.java
@@ -4,11 +4,15 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class HelixIPCMessageCodecRegistry {
+    private static final int FIRST_CUSTOM_MESSAGE_TYPE = 1024;
 
     private final ConcurrentMap<Integer, HelixIPCMessageCodec> registry
             = new ConcurrentHashMap<Integer, HelixIPCMessageCodec>();
 
     public void put(int messageType, HelixIPCMessageCodec codec) {
+        if (messageType < FIRST_CUSTOM_MESSAGE_TYPE) {
+            throw new IllegalArgumentException("First allowed custom message type is " + FIRST_CUSTOM_MESSAGE_TYPE);
+        }
         registry.put(messageType, codec);
     }
 
